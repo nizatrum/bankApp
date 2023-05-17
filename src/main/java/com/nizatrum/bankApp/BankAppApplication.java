973b983a766701c901775002f6dfe053d9ee3b2a
Timur <timur.murtazin434@gmail.com>
2023-05-18 00:04:36 +0400

    before lesson 9

--- src/main/java/com/nizatrum/bankApp/BankAppApplication.java
+++ src/main/java/com/nizatrum/bankApp/BankAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BankAppApplication.class, args);
 	}
