2b6689544c317941c6c105c8f5179abe37aadaf8
Timur <timur.murtazin434@gmail.com>
2023-07-12 00:31:58 +0400

    алгорит луна реализован, сделал кастомную страницу логина

--- src/main/java/com/nizatrum/bankApp/configurations/SecurityConfiguration.java
+++ src/main/java/com/nizatrum/bankApp/configurations/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -42,6 +43,7 @@ public class SecurityConfiguration {
                         .requestMatchers("/client", "/account").hasAuthority("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin((form) -> form
+                        .loginPage("/login")
                         .usernameParameter("username")
                         .passwordParameter("password")
                         .permitAll())
